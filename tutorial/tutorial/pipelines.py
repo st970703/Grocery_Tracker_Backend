e009305c938ede493dc3d7bda4140cf4449db665
st970703 <st970703@users.noreply.github.com>
2021-07-21 17:21:15 +1200

    Added pymongo integration

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -5,7 +5,19 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
 
 class TutorialPipeline(object):
+
+    def __init__(self):
+        self.conn = pymongo.MongoClient(
+            'localhost',
+            27017
+        )
+
+        db = self.conn['my_groceries']
+        self.collection = db['paknsave_tb']
+
     def process_item(self, item, spider):
+        self.collection.insert(dict(item))
         return item
